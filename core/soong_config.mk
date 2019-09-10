c9f35fe19381e4060d75dd3e1d15f1ce65a709e0
Han Wang <416810799@qq.com>
2019-09-10 13:08:04 +0200

    soong_config: Add new flag for vendor_init
    Change-Id: I383f1de230ecf7b79d78fe701b140bd1bc6795f2
    Signed-off-by: Nishant Kumar <www.rajsonu13@gmail.com>

--- core/soong_config.mk
+++ core/soong_config.mk
@@ -316,6 +316,8 @@ $(call add_json_list, SepolicyFreezeTestExtraPrebuiltDirs, $(SEPOLICY_FREEZE_TES
 
 $(call add_json_bool, GenerateAidlNdkPlatformBackend, $(filter true,$(NEED_AIDL_NDK_PLATFORM_BACKEND)))
 
+$(call add_json_str_omitempty, Target_init_vendor_lib, $(TARGET_INIT_VENDOR_LIB))
+
 $(call add_json_bool, IgnorePrefer32OnDevice, $(filter true,$(IGNORE_PREFER32_ON_DEVICE)))
 
 $(call add_json_list, IncludeTags,                $(PRODUCT_INCLUDE_TAGS))
